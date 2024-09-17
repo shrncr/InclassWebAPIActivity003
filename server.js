f661b2d2d92b5b4c0d9deb529a81a6348a4fdf92
shrncr <loveyawink@gmail.com>
2024-09-17 14:48:14 -0500

    Feat -- create required classes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// server.js
+
 // Import required modules
 const express = require('express');
 const path = require('path');
@@ -9,14 +11,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'public' folder
-app.use(express.static(path.join(dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route to serve the main index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(dirname, 'public', 'webapiactivity.html'));
+  res.sendFile(path.join(__dirname, 'public', 'webapiactivity.html'));
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:${PORT}");
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
